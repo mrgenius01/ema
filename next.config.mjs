3623edbe8f9a3cafb51b9959e87f61f3e0883c62
Anesu Ndachengedzwa <anesundache@gmail.com>
2024-10-11 01:29:21 +0200

    fix: build error\ removed outDir

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Set a custom output directory (not 'public')
-    distDir: 'out',
   
     // Enable React Strict Mode
     reactStrictMode: true,
