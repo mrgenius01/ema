37411d0993adac7856631f068a07db0ab83eb205
Anesu Ndachengedzwa <anesundache@gmail.com>
2024-10-14 15:33:19 +0200

    fix: login/

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -40,6 +40,7 @@ export async function POST(request: Request) {
       },
     })
   } catch (error) {
+    console.log(error)
     return NextResponse.json(
       { error: 'Internal server error' },
       { status: 500 }
