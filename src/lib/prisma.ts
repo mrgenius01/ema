19e87e921ca6b70b78e33f0046830f181540dea3
Anesu Ndachengedzwa <anesundache@gmail.com>
2024-10-14 16:00:41 +0200

    fix: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-declare global {
-  const prisma: PrismaClient | undefined
+// declare global {
+//   const prisma: PrismaClient | undefined
+// }
+
+// export const prisma = global.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+import { PrismaClient } from '@prisma/client';
+
+declare const global: typeof globalThis & { prisma?: PrismaClient };
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
 }
-
-export const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
-
